4f3437f242db419786e059c52b7ad52cb2c178ff
xxzj990 <xxzj990@gmail.com>
2014-04-27 13:21:17 +0800

    Remove no use import.

--- ViewPagerDemo/src/com/xxzj990/android/viewpagerdemo/MainActivity.java
+++ ViewPagerDemo/src/com/xxzj990/android/viewpagerdemo/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
